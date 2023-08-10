4cabaf644ff0f73e0cdc3cf475ecb5dbb72817ee
Alessandro Hugen <alessandrohugen@gmail.com>
2023-08-10 17:23:07 -0300

    feat: create basic home header

--- src/Views/Home.vue
+++ src/Views/Home.vue
@@ -3,8 +3,20 @@ import { computed } from 'vue'
 import store from '../store'
 
 const meals = computed(() => store.state.meals)
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
 </script>
 
 <template>
-  <pre>{{ meals }}</pre>
+  <header class="flex flex-col p-8">
+    <input
+      type="text"
+      class="rounded border-2 border-gray-200 w-full"
+      placeholder="Busque receitas"
+    />
+    <section class="flex gap-2 justify-center mt-2">
+      <router-link to="/" v-for="letter of letters">
+        {{ letter }}
+      </router-link>
+    </section>
+  </header>
 </template>
