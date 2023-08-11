2a0540aab4e35087967b573811f88a68ebb165c6
Alessandro Hugen <alessandrohugen@gmail.com>
2023-08-11 00:15:52 -0300

    feat: create navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,31 @@
 <script setup lang="ts"></script>
 
 <template>
-  <router-view />
+  <div class="bg-gray-100 h-full">
+    <header class="bg-white shadow-md h-16 flex justify-between items-stretch">
+      <router-link :to="{ name: 'home' }"> Home </router-link>
+      <nav class="flex items-center gap-1">
+        <router-link
+          to="/"
+          class="inline-flex px-2 h-full transition-colors hover:bg-orange-400"
+          >Pesquisar Receitas</router-link
+        >
+        <router-link
+          to="/"
+          class="inline-flex px-2 h-full transition-colors hover:bg-orange-400"
+          >Receitas por Letras</router-link
+        >
+        <router-link
+          to="/"
+          class="inline-flex px-2 h-full transition-colors hover:bg-orange-400"
+          >Receitas por Ingredientes</router-link
+        >
+      </nav>
+    </header>
+    <main>
+      <router-view />
+    </main>
+  </div>
 </template>
 
 <style scoped></style>
