5631f9521f32726d648c457ac6ef31b6897061af
Alessandro Hugen <alessandrohugen@gmail.com>
2023-08-15 21:36:11 -0300

    feat: add new filter pahe

--- src/Views/MealsByLetter.vue
+++ src/Views/MealsByLetter.vue
@@ -1,5 +1,20 @@
 <script setup lang="ts">
+import { computed, onMounted, watch } from 'vue'
+import store from '../store'
+import { useRoute } from 'vue-router'
+import MealItem from '../components/MealItem.vue'
+
 const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+const meals = computed(() => store.state.mealsByLetter)
+const route = useRoute()
+
+watch(route, () => {
+  store.dispatch('searchMealsByLetter', route.params.letter)
+})
+
+onMounted(() => {
+  store.dispatch('searchMealsByLetter', route.params.letter)
+})
 </script>
 
 <template>
@@ -11,5 +26,9 @@ const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
     >
       {{ letter }}
     </router-link>
+  </section>
+
+  <section class="grid grid-cols-1 md:grid-cols-5 gap-3 p-8">
+    <MealItem v-for="meal of meals" :key="meal.idMeal" :meal="meal" />
   </section>
 </template>
--- src/store/actions.ts
+++ src/store/actions.ts
@@ -6,11 +6,11 @@ export async function searchMeals({ commit }: any, keyword: string) {
 }
 
 export async function searchMealsByLetter({ commit }: any, letter: string) {
-  const { data } = await api.get(`/search.php?s=${letter}`)
+  const { data } = await api.get(`/search.php?f=${letter}`)
   commit('setMealsByLetter', data.meals)
 }
 
 export async function searchMealsByIngredient({ commit }: any, ingredient: string) {
-  const { data } = await api.get(`/search.php?s=${ingredient}`)
+  const { data } = await api.get(`/filter.php?i=${ingredient}`)
   commit('setMealsByIngredient', data.meals)
 }
